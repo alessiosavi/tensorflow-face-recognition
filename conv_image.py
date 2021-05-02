5a5f2ccd308529012e88ca8af8a6b1952dc8b4d5
alessiosavi <alo.savi@fabricalab.it>
2021-05-02 20:57:10 +0200

    v0.0.1  - Example on how to perform face recognition with DLIB + Tensorflow [~98% of accuracy]

---
+++ conv_image.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:163d40f160a3feb1050b87c16e9d44825eeb52bdc00705a2bd3f095fe9d5ded8
+size 8251
---
+++ dense_embedding.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa5b6003a022624a94b4ebba8302a3b694221ca19309e35fced37c526aa1b6bf
+size 5863
